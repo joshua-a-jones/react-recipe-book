6f5321bf681ff43d491ec97e5b80fa89e41f679c
joshuajones <jjones@datagration.com>
2021-12-17 15:52:29 -0600

    Added Searchbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import './Navbar.css';
 import React from 'react'
 import { Link } from 'react-router-dom'
+import  Searchbar from '../Searchbar/Searchbar'
 
 export default function Navbar() {
     return (
@@ -9,7 +10,11 @@ export default function Navbar() {
                 <Link to='/' className='logo'>
                     <h1>My Recipe Book</h1>
                 </Link>
-                <Link to='/create'>Create Recipe</Link>
+                <div className='h-stack'>
+                    <Searchbar />
+                    <Link to='/create'>Create Recipe</Link>
+                </div>
+
             </nav>
         </div>
     )
---
+++ src/components/Searchbar/Searchbar.tsx
@@ -0,0 +1,27 @@
+import './Searchbar.css';
+import React, { useState, FormEvent } from 'react'
+import { useHistory } from 'react-router-dom';
+
+export default function Searchbar() {
+    const [searchText, setSearchText] = useState('');
+    const history = useHistory();
+    
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        history.push(`/search?q=${encodeURIComponent(searchText)}`)
+    }
+    return (
+        <div className="searchbar">
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="search">Search:</label>
+                <input
+                    type='text'
+                    id='search'
+                    onChange={(e) => setSearchText(e.target.value)}
+                    value={searchText}
+                />
+            </form>
+        </div>
+    )
+}
