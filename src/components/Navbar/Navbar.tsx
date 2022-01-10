0ce2c0fad2905c28cf1563ec3358de7bb46597bd
joshuajones <jjones@datagration.com>
2022-01-10 11:03:07 -0600

    Created ThemeContext component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,10 +2,13 @@ import './Navbar.css';
 import React from 'react'
 import { Link } from 'react-router-dom'
 import  Searchbar from '../Searchbar/Searchbar'
+import { useTheme } from '../../api/hooks/useTheme';
 
 export default function Navbar() {
+    const { state } = useTheme();
+
     return (
-        <div className='navbar'>
+        <div className='navbar' style={{background: state.color}}>
             <nav>
                 <Link to='/' className='logo'>
                     <h1>My Recipe Book</h1>
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,15 +1,45 @@
-import React, { createContext } from "react";
+import React, { createContext, useReducer } from "react";
 
-const light = {
-    color: 'blue'
+
+export interface IThemeContext {
+    state: ThemeState;
+    changeColor: (x: string) => void
 }
 
-export const ThemeContext = createContext(light);
+export const ThemeContext = createContext<IThemeContext | undefined>(undefined);
+
+enum ThemeActions {
+    CHANGE_COLOR = 'CHANGE_COLOR'
+}
+
+export type ThemeState = {
+    color: string
+}
+
+export type ThemeAction = {
+    type: ThemeActions;
+    payload: string;
+}
+
+const themeReducer = (state: ThemeState, action: ThemeAction) => {
+    switch (action.type) {
+        case ThemeActions.CHANGE_COLOR:
+            return {...state, color: action.payload}
+        default:
+            return state
+    }
+}
+
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
+    const [state, dispatch] = useReducer(themeReducer, {color: 'teal'});
+
+    const changeColor = (color: string) => {
+        dispatch({type: ThemeActions.CHANGE_COLOR, payload: color});
+    }
 
     return (
-        <ThemeContext.Provider value={{color: 'blue'}}>
+        <ThemeContext.Provider value={{state, changeColor}}>
             {children}
         </ThemeContext.Provider>
     )
