651f79ff4524e033935fbebaf149c8acbbae5cce
Joshua Jones <joshua.desjones@gmail.com>
2022-05-05 10:45:15 -0500

    formatting and some styling on searchbar

--- src/components/Searchbar/Searchbar.tsx
+++ src/components/Searchbar/Searchbar.tsx
@@ -1,28 +1,30 @@
-import './Searchbar.css';
-import { useState, FormEvent } from 'react'
-import { useHistory } from 'react-router-dom';
+import "./Searchbar.css";
+import { useState, FormEvent } from "react";
+import { useHistory } from "react-router-dom";
+import { useTheme } from "../../api/hooks/useTheme";
 
 export default function Searchbar() {
-    const [searchText, setSearchText] = useState('');
-    const history = useHistory();
-    
-    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+  const [searchText, setSearchText] = useState("");
+  const history = useHistory();
+  const { themeStyle } = useTheme();
 
-        history.push(`/search?q=${encodeURIComponent(searchText)}`)
-        setSearchText('');
-    }
-    return (
-        <div className="searchbar">
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="search">Search:</label>
-                <input
-                    type='text'
-                    id='search'
-                    onChange={(e) => setSearchText(e.target.value)}
-                    value={searchText}
-                />
-            </form>
-        </div>
-    )
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    history.push(`/search?q=${encodeURIComponent(searchText)}`);
+    setSearchText("");
+  };
+  return (
+    <div className={`searchbar ${themeStyle.mode}`}>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="search">Search:</label>
+        <input
+          type="text"
+          id="search"
+          onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
+        />
+      </form>
+    </div>
+  );
 }
