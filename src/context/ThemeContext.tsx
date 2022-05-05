23ef75ad2214df6b1a3e1870699b933feb66c1dc
Joshua Jones <joshua.desjones@gmail.com>
2022-05-05 10:44:32 -0500

    formatting fixes

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,64 +1,62 @@
 import React, { createContext, useReducer } from "react";
 
-
 export interface IThemeContext {
-    themeStyle: ThemeStyleState;
-    changeColor: (x: string) => void
-    changeMode: (mode: 'light' | 'dark') => void
+  themeStyle: ThemeStyleState;
+  changeColor: (color: string) => void;
+  changeMode: (mode: "light" | "dark") => void;
 }
 
 export const ThemeContext = createContext<IThemeContext | undefined>(undefined);
 
 export type ThemeStyleState = {
-    background: string
-    mode: string
-}
+  background: string;
+  mode: string;
+};
 
 export type ThemeAction = {
-    type: ThemeActions;
-    payload: string;
-}
+  type: ThemeActions;
+  payload: string;
+};
 
 enum ThemeActions {
-    CHANGE_COLOR = 'CHANGE_COLOR',
-    CHANGE_MODE = 'CHANGE_MODE'
+  CHANGE_COLOR = "CHANGE_COLOR",
+  CHANGE_MODE = "CHANGE_MODE",
 }
 
 const themeReducer = (state: ThemeStyleState, action: ThemeAction) => {
-    switch (action.type) {
-        case ThemeActions.CHANGE_COLOR:
-            return {...state, background: action.payload}
-        case ThemeActions.CHANGE_MODE:
-            return {...state, mode: action.payload}
-        default:
-            return state
-    }
-}
-
+  switch (action.type) {
+    case ThemeActions.CHANGE_COLOR:
+      return { ...state, background: action.payload };
+    case ThemeActions.CHANGE_MODE:
+      return { ...state, mode: action.payload };
+    default:
+      return state;
+  }
+};
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-    const [themeStyle, dispatch] = useReducer(themeReducer, {
-        background: 'teal',
-        mode: 'light'
+  const [themeStyle, dispatch] = useReducer(themeReducer, {
+    background: "teal",
+    mode: "light",
+  });
+
+  const changeColor = (color: string) => {
+    dispatch({
+      type: ThemeActions.CHANGE_COLOR,
+      payload: color,
     });
+  };
 
-    const changeColor = (color: string) => {
-        dispatch({
-            type: ThemeActions.CHANGE_COLOR,
-            payload: color
-        });
-    }
+  const changeMode = (mode: "light" | "dark") => {
+    dispatch({
+      type: ThemeActions.CHANGE_MODE,
+      payload: mode,
+    });
+  };
 
-    const changeMode = (mode: 'light' | 'dark') => {
-        dispatch({
-            type: ThemeActions.CHANGE_MODE,
-            payload: mode
-        });
-    }
-
-    return (
-        <ThemeContext.Provider value={{themeStyle, changeColor, changeMode}}>
-            {children}
-        </ThemeContext.Provider>
-    )
+  return (
+    <ThemeContext.Provider value={{ themeStyle, changeColor, changeMode }}>
+      {children}
+    </ThemeContext.Provider>
+  );
 }
