ecf8deb612d2b1231833545416013aec3b3e7834
Joshua Jones <joshua.desjones@gmail.com>
2022-05-06 11:42:59 -0500

    added search capability back

--- src/pages/search/Search.tsx
+++ src/pages/search/Search.tsx
@@ -1,29 +1,60 @@
-import './Search.css';
-import { useLocation } from 'react-router-dom';
-import { useAxios } from '../../api/hooks/useAxios';
-import { IRecipe } from '../../api/recipes/Recipe';
-import RecipeList from '../../components/RecipeList/RecipeList';
-import { useTheme } from '../../api/hooks/useTheme';
+import "./Search.css";
+import { useLocation } from "react-router-dom";
+import { IRecipe } from "../../api/recipes/Recipe";
+import RecipeList from "../../components/RecipeList/RecipeList";
+import { useTheme } from "../../api/hooks/useTheme";
+import { projectFirestore } from "../../firebase/config";
+import "firebase/firestore";
+import { useEffect, useState } from "react";
 
 export default function Search() {
-    const { themeStyle } = useTheme()
-    const location = useLocation()
-    const queryParams = new URLSearchParams(location.search)
-    const query = queryParams.get('q');
+  const { themeStyle } = useTheme();
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const query = queryParams.get("q");
 
-    const url = 'http://localhost:3000/recipes?q=' + query;
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIserror] = useState(false);
+  const [matchingRecipes, setMatchingRecipes] = useState<
+    IRecipe[] | undefined
+  >();
 
-    const {responseData, isLoading, isError} = useAxios<IRecipe[]>({
-        method: 'get',
-        url: url
-    })
+  useEffect(() => {
+    projectFirestore
+      .collection("recipes")
+      .get()
+      .then((collection) => {
+        if (collection.empty) {
+          setIserror(true);
+          setIsLoading(false);
+        } else {
+          let results: IRecipe[] = [];
+          collection.docs.forEach((doc) => {
+            results.push({ id: doc.id, ...doc.data() } as IRecipe);
+          });
+          setIsLoading(false);
+          if (query) {
+            setMatchingRecipes(
+              results.filter(
+                (recipe) =>
+                  recipe.title.includes(query) || recipe.method.includes(query)
+              )
+            );
+          }
+        }
+      })
+      .catch((error) => {
+        setIserror(true);
+        setIsLoading(false);
+      });
+  }, [query]);
 
-    return (
-        <div className={`search ${themeStyle.mode}`}>
-            <h2 className='page-title'>Recipes including "{query}"</h2>
-            {isError && <p className='error'>Could Not Load Recipes</p>}
-            {isLoading && <p className='loading'>Loading...</p>}
-            {responseData && <RecipeList recipes={responseData}/>}
-        </div>
-    )
+  return (
+    <div className={`search ${themeStyle.mode}`}>
+      <h2 className="page-title">Recipes including "{query}"</h2>
+      {isError && <p className="error">Could Not Load Recipes</p>}
+      {isLoading && <p className="loading">Loading...</p>}
+      {matchingRecipes && <RecipeList recipes={matchingRecipes} />}
+    </div>
+  );
 }
