6883e5fcd82a43e4fcbfa578c6be4f9a7cbb68b0
Joshua Jones <joshua.desjones@gmail.com>
2022-05-07 11:21:56 -0500

    made navbar more responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,38 @@
-import './Navbar.css';
-import React from 'react'
-import { Link } from 'react-router-dom'
-import  Searchbar from '../Searchbar/Searchbar'
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Searchbar from "../Searchbar/Searchbar";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { useTheme } from '../../api/hooks/useTheme';
+import { useTheme } from "../../api/hooks/useTheme";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
-    return (
-        <div className='navbar' >
-            <nav>
-                <Link to='/' className='logo'>
-                    <h1>My Recipe Book</h1>
-                </Link>
-                <div className='h-stack'>
-                    <Searchbar />
-                    <Link to='/create'>Create Recipe</Link>
-                </div>
-
-            </nav>
+  const handleHamburgerClick = () => {
+    setIsHamburgerOpen(!isHamburgerOpen);
+  };
+  return (
+    <div className="navbar">
+      <nav>
+        <Link to="/" className="logo">
+          <h1>My Recipe Book</h1>
+        </Link>
+        <div className="h-stack desktop-nav">
+          <Searchbar />
+          <Link to="/create">Create Recipe</Link>
         </div>
-    )
+
+        <div className="mobile-nav">
+          <GiHamburgerMenu onClick={handleHamburgerClick} />
+        </div>
+      </nav>
+      {isHamburgerOpen && (
+        <div className="hamburger-menu">
+          <Searchbar />
+          <Link to="/create">Create Recipe</Link>
+        </div>
+      )}
+    </div>
+  );
 }
