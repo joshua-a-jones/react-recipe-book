44f5ca355b231e5339ba46b4ca776e82d43438ec
Joshua Jones <joshua.desjones@gmail.com>
2022-05-09 08:46:40 -0500

    added validation to registration form

--- src/pages/registration/Registration.tsx
+++ src/pages/registration/Registration.tsx
@@ -7,15 +7,31 @@ import "./Registration.css";
 export default function Registration() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+  const [isPasswordValid, setIsPasswordValid] = useState(false);
   const history = useHistory();
   const { themeStyle } = useTheme();
 
   const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    projectAuth.createUserWithEmailAndPassword(email, password).then((cred) => {
-      history.push("/");
-    });
+    if (isPasswordValid && isEmailValid) {
+      projectAuth
+        .createUserWithEmailAndPassword(email, password)
+        .then((cred) => {
+          history.push("/");
+        });
+    }
+  };
+
+  const handlEmailInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setEmail(e.currentTarget.value);
+    setIsEmailValid(!e.currentTarget.validity.typeMismatch);
+  };
+
+  const handlPasswordInputChange = (e: FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+    setIsPasswordValid(!e.currentTarget.validity.patternMismatch);
   };
 
   return (
@@ -26,9 +42,10 @@ export default function Registration() {
           <span>Email</span>
           <input
             id="email-input"
-            type="text"
+            type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => handlEmailInputChange(e)}
+            required
           />
         </label>
         <label>
@@ -37,8 +54,9 @@ export default function Registration() {
             id="password-input"
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => handlPasswordInputChange(e)}
             minLength={6}
+            required
           />
         </label>
         <button type="submit">Submit</button>
